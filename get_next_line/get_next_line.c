53ae75299624710431b7cd715b0b177abaa7c759
Tiwong Lenovo p11 <tcwongj@gmail.com>
2023-10-12 10:27:36 +0000

    first try at get_next_line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tiwong <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 16:41:55 by tiwong            #+#    #+#             */
-/*   Updated: 2023/10/08 00:05:04 by tiwong           ###   ########.fr       */
+/*   Updated: 2023/10/11 09:18:27 by tiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,38 @@
 
 char	*get_next_line(int fd)
 {
-	char	*buffer;
+	char	buffer[BUFFER_SIZE];
 	ssize_t	p_read;
-	char	*temp;
-
-		
-	buffer = (char *)malloc(ft_strlen(temp) * sizeof (char)); 
+	static int	i;
+	int	count;
+	char	str[BUFFER_SIZE];
+	
+	buffer = (char *)malloc((buffer + 1) * sizeof (char)); 
 	p_read = read(fd, buffer, sizeof (buffer));
+	if (p_read == -1)
+		return (NULL);
+	i = 0;
+	count = 0;
+	if (buffer[i++] == '\n')
+	{
+		str[count++] = '\n';
+		return ("\n");
+	}
+	while (buffer[i] != '\n')
+		str[count++] = buffer[i++];
+	str[count] = '\n';
+	str[count + 1] = '\0';
+	return (str);			
+}
+
+#include <stdio.h>
+int	main(void)
+{
+	int	file_desc;
+	char	*get_next_line;
+
+	file_desc = open("test.txt", O_RDONLY);
+	if (file_desc == -1)
+		return (-1);
+	while (*get_next_line > 0)
 
